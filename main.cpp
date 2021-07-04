7138b6915bdc654a2b1b1c07610cb0f56d218ef6
weikai <569022027@qq.com>
2021-07-04 23:25:43 +0800

    创建main.cpp代码写了初始的代码

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include<.iostream>
+using namespace std;
+
+viod sort(int arr[],int size)
+{
+	
+	
+}
+
+int main()
+{
+	int arr[]={12,4,89,43,21,78};
+	int size=sizeof(arr)/sizeof(arr[0]);
+	sort(arr,size);
+	return 0;
+	
+	
+}
