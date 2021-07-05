1402063a28f8fdc16af7fc9db5324b69799412ea
weikai <569022027@qq.com>
2021-07-05 18:17:34 +0800

    实现sort函数

--- main.cpp
+++ main.cpp
@@ -4,7 +4,20 @@ using namespace std;
 viod sort(int arr[],int size)
 {
 	
-	
+   
+	int temp=0;
+	for(int i=0;i<size-1;++i)
+	{
+	for(int j=0;j<size-1-i;++j)
+	{
+	if(arr[j]>arr[j+1])
+	{
+	temp=arr[j];
+	arr[j]=arr[j+1];
+	arr[j]=temp;
+	}
+	}
+	}
 }
 
 int main()
@@ -16,3 +29,4 @@ int main()
 	
 	
 }
+
