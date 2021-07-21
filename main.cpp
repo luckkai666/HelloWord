4db1c1743dc82653e9b22c04598f2272def6226e
unknown <569022027@qq.com>
2021-07-21 11:03:09 +0800

    修改sort算法为快排分割算法

--- main.cpp
+++ main.cpp
@@ -1,23 +1,36 @@
 #include<.iostream>
 using namespace std;
+void sort(int arr[],int i,int j)
+{
+	int val=[i];
+	int l=i;
+	int r=j;
+	while(l<r)
+	{while(l<r&&arr[r]>=val)
+	{
+		r--;
+	}
+	if(l<r)
+	{
+		arr[l++]=arr[r];
+	}
+	while(l<r&&arr[l]<=val)
+	{
+		l++;
+	}
+	if(l<r)
+	{
+		arr[r--]=arr[l];
+	}
+		}
+	arr[l]=val;
+	sort(arr,i,l-1);
+	sort(arr,l+1,j);
+}
 
 viod sort(int arr[],int size)
 {
-	
-   
-	int temp=0;
-	for(int i=0;i<size-1;++i)
-	{
-	for(int j=0;j<size-1-i;++j)
-	{
-	if(arr[j]>arr[j+1])
-	{
-	temp=arr[j];
-	arr[j]=arr[j+1];
-	arr[j]=temp;
-	}
-	}
-	}
+sort(arr,0,size);
 }
 
 int main()
@@ -25,8 +38,11 @@ int main()
 	int arr[]={12,4,89,43,21,78};
 	int size=sizeof(arr)/sizeof(arr[0]);
 	sort(arr,size);
+	for(int i=0;i<size;++i)
+	{
+		cout<<arr[i]<<" ";
+	}
+	cout<<endl;
 	return 0;
-	
-	
 }
 
